44f31bf84e863922a3943914d163fb68ee7c98ff
Stephen Ecsedy <stephen.ecsedy@gmail.com>
2015-11-09 17:09:03 -0500

    Add Base Code
    Adding in Tab Based Application.  Base Code, XCODE Generated

---
+++ PowerRangers/PowerRangers/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PowerRangers
+//
+//  Created by Tommy on 11/9/15.
+//  Copyright © 2015 Tommy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
