a23f5d19b025d63ac340d382586373b86f877609
Patrick O'Brien <pbobrien0@gmail.com>
2015-11-17 22:23:10 -0500

    Tab Button Connection to Third View
    Three tab buttons on bottom navigation instead of two. No image or
    icon, but it changes pages

--- PowerRangers/PowerRangers/JSON ViewController.swift
+++ PowerRangers/PowerRangers/JSON ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         
+        
+        
+        
         var names = [String]()
         var tracks = [String]()
         var roles = [String]()
