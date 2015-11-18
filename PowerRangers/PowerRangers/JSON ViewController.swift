50608003593d0d8e8016c7f1250cd48d0c7df722
Patrick O'Brien <pbobrien0@gmail.com>
2015-11-17 21:26:36 -0500

    Adding and Pulling JSON
    Added JSON file and added code to pull from all categories (not
    connected to storyboard)

--- PowerRangers/PowerRangers/JSON ViewController.swift
+++ PowerRangers/PowerRangers/JSON ViewController.swift
@@ -13,22 +13,46 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         
         var names = [String]()
+        var tracks = [String]()
+        var roles = [String]()
+        var locations = [String]()
+        var interestsDict = [String]()
+        
         DataManager.grabNames { (data) -> Void in
-        do {
-            let json = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
-            
-            if let blps = json["blps"] as? [[String: AnyObject]] {
-                for blps in blps {
-                    if let firstName = blps["firstName"] as? String {
-                        names.append(firstName)
+            do {
+                let json = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+                
+                if let blps = json["blps"] as? [[String: AnyObject]] {
+                    for blps in blps {
+                        // Pulling first and last names and combining into one dictionary
+                        if let firstName = blps["firstName"] as? String {
+                            if let lastName = blps["lastName"] as? String {
+                                names.append(firstName + " " + lastName)
+                            }
+                        }
+                        // pulling track info into separate dictionary
+                        if let track = blps["track"] as? String {
+                            tracks.append(track)
+                        }
+                        // pulling role into separate dictionary
+                        if let role = blps["role"] as? String {
+                            roles.append(role)
+                        }
+                        //pulling locations into dictionary
+                        if let location = blps["location"] as? String {
+                            locations.append(location)
+                        }
+                        // pull interests
+                        if let interests = blps["interests"] as? String {
+                            interestsDict.append(interests)
+                        }
                     }
                 }
+            } catch {
+                print("error serializing JSON: \(error)")
             }
-        } catch {
-            print("error serializing JSON: \(error)")
-            }
-        
-        print(names) // ["Bloxus test", "Manila Test"]
+            
+            print(interestsDict)
         
         }
         
