94632a86a9fbc4f2b12bebcd879f7943a5769d1a
Daniel Garofalo <danielpaulgarofalo@gmail.com>
2015-11-23 21:35:51 -0500

    Adding "ViewMyProfile" page to storyboard
    Added scoreboard page and view controller. no buttons added to
    controller

---
+++ PowerRangers/PowerRangers/ViewMyProfile.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  My Profile
+//
+//  Created by Jason  on 11/23/15.
+//  Copyright © 2015 Jason . All rights reserved.
+//
+
+import UIKit
+
+class ViewMyProfile: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
+
