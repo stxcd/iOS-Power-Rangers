1398dbda1ccb00a98f28c12c3665e5bbc8e808a5
Patrick O'Brien <pbobrien0@gmail.com>
2015-11-23 20:03:06 -0500

    Created viewMyHousing.swift
    Needed file to have storyboard load properly.

---
+++ PowerRangers/PowerRangers/viewMyHousingProfile.swift
@@ -0,0 +1,24 @@
+//
+//  viewMyHousingProfile.swift
+//  PowerRangers
+//
+//  Created by Zack on 11/23/15.
+//  Copyright © 2015 Tommy. All rights reserved.
+//
+
+import UIKit
+
+class ViewMyHousingProfile: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
