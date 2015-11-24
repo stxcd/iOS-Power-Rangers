627294900df737acb5feed00ffe001a2331f9371
Stephen Ecsedy <stephen.ecsedy@gmail.com>
2015-11-23 21:01:07 -0500

    New FAQ Addition
    Changed the layout and structure of FAQ ViewController, and added code
    on FAQViewController page

--- PowerRangers/PowerRangers/FAQViewController.swift
+++ PowerRangers/PowerRangers/FAQViewController.swift
@@ -9,6 +9,25 @@
 import UIKit
 
 class FAQViewController: UIViewController {
+    
+    //MARK: Properties
+    
+    var question1 = "What is Lorum Ipsum?"
+    var question2 = "Why do we use it?"
+    var question3 = "Where does it come from?"
+    var question4 = "Where can I get some?"
+    
+    var answer1 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    
+    var answer2 = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
+    
+    var answer3 = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, 'Lorem ipsum dolor sit amet..', comes from a line in section 1.10.32."
+    
+    var answer4 = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var answerLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +39,27 @@ class FAQViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //MARK: Actions
+    
+    @IBAction func question1Button(sender: UIButton) {
+        questionLabel.text = question1
+        answerLabel.text = answer1
+    }
+    
+    @IBAction func question2Button(sender: UIButton) {
+        questionLabel.text = question2
+        answerLabel.text = answer2
+    }
 
+    @IBAction func question3Button(sender: UIButton) {
+        questionLabel.text = question3
+        answerLabel.text = answer3
+    }
+    
+    @IBAction func question4Button(sender: UIButton) {
+        questionLabel.text = question4
+        answerLabel.text = answer4
+    }
+    
 }
 
