cec04e56dd7ba1bbcdd4da097d216702411720b2
katiesenk <katherine.senk@synchronyfinancial.com>
2015-11-24 09:27:34 -0500

    edit my profile
    Storyboard and code changes for edit my profile

--- PowerRangers/PowerRangers/ViewMyProfile.swift
+++ PowerRangers/PowerRangers/ViewMyProfile.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewMyProfile: UIViewController {
     
+    // MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var classLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var aboutLabel: UILabel!
+    @IBOutlet weak var trackLabel: UILabel!
+    @IBOutlet weak var roleLabel: UILabel!
+    @IBOutlet weak var nextLocation: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
