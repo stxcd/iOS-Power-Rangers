9353983e3aa50294e6a4b8443a42cbb9e4a6f38a
Trace Pomplun <trace.pomplum@gpshopper.com>
2015-12-08 11:26:32 -0500

    Synchrony yellow

---
+++ PowerRangers/PowerRangers/UIColor+Synchrony.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Synchrony.swift
+//  SynchronyEvents
+//
+//  Created by James Lee on 6/15/15.
+//  Copyright (c) 2015 gpshopper. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    class func synchronyYellow() -> UIColor {
+        return UIColor(red: 248.0/255.0, green: 193.0/255.0, blue: 10.0/255.0, alpha: 1)
+    }
+}
